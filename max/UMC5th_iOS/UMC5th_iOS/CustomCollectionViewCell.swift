0ca621d56b4eaf2144bddb9bb420fc38f75b623a
seonwoonam <potatomoney716@gmail.com>
2023-10-09 17:12:26 +0900

    [FEAT] : 2주차 실습 당근마켓 스크롤 뷰 클론
    좌우 스크롤은 아직 구현 안됨

---
+++ max/UMC5th_iOS/UMC5th_iOS/CustomCollectionViewCell.swift
@@ -0,0 +1,16 @@
+//
+//  CustomCollectionViewCell.swift
+//  UMC5th_iOS
+//
+//  Created by seonwoo on 2023/10/04.
+//
+
+import UIKit
+
+class CustomCollectionViewCell: UICollectionViewCell {
+    
+    @IBOutlet weak var image: UIImageView!
+    @IBOutlet weak var name: UILabel!
+    @IBOutlet weak var location: UILabel!
+    @IBOutlet weak var price: UILabel!
+}
---
+++ max/UMC5th_iOS/UMC5th_iOS/Product.swift
@@ -0,0 +1,15 @@
+//
+//  Product.swift
+//  UMC5th_iOS
+//
+//  Created by seonwoo on 2023/10/04.
+//
+
+import Foundation
+
+struct Product {
+    var name : String
+    var image : String
+    var location : String
+    var howMuch : String
+}
